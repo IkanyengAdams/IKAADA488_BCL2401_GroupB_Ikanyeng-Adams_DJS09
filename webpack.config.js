b9fac0265760ff16d41e43c17c40b5416adbb5ea
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-06-10 11:51:00 +0200

    Updated index.html to reference script from dist folder, added showReviewTotal function to index.ts, updated dependencies and versions in package.json, and modified webpack.config.js to include output path

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
+const path = require('path');
 module.exports = {
   "output": {
-    "filename": "[name].pack.js"
+    "filename": "[name].pack.js",
+    path: path.resolve(__dirname, 'dist'),
   },
   "entry": {
     "index": "./index"
