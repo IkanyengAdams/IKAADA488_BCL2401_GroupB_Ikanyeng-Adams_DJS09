1202012ef7c33bbe368058f2e9c81a04b32fa64d
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-06-24 15:55:40 +0200

    corrected my webpack files

--- dist/index.pack.js
+++ dist/index.pack.js
@@ -1,1 +1,3 @@
 (()=>{var e,a=document.querySelector("#reviews"),t=[{name:"Sheia",stars:5,loyaltyUser:!0,date:"01-04-2021"},{name:"Andrzej",stars:3,loyaltyUser:!1,date:"28-03-2021"},{name:"Omar",stars:4,loyaltyUser:!0,date:"27-03-2021"}];e=t.length,t[0].name,a.innerHTML="review total "+e.toString()})();
+
+<script src="/dist/index.pack.js"></script>
